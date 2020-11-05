5f57b3f34142dfba54adc1bec1b359d4afe5c865
Arconycompany <arconycompany@gmail.com>
2020-11-05 20:21:10 +0100

    'first'

---
+++ GameServer/GameServer/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Constants
+    {
+       public const int TICKS_PER_SEC = 30;
+        public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
+    }
+}
---
+++ GameServer/GameServer/GameLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class GameLogic
+    {
+        public static void Update()
+        {
+
+            foreach (Client _client in Server.clients.Values)
+            {
+                if (_client.player != null)
+                {
+                    _client.player.Update();
+                }
+            }
+            ThreadManager.UpdateMain();
+        }
+    }
+}
